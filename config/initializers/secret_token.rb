68d6949d19027c2629bd9f1a3fb2188571e36c76
Christopher Dunn <cdunn2001@gmail.com>
2011-01-15 23:14:53 -0800

    After 'rails new .'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoyLevi::Application.config.secret_token = 'dd72b45a58f81ddadecba314a6c7a88ec2f4238b8b3d61d860497bb871aa189f1865b6431a7ac6fea12757d678453f46e53ae0a4032753216b1788d9a8418b5c'
