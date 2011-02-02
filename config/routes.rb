96253d10f95020016e4dbe0c204b75b5e4491972
Christopher Dunn <cdunn2001@gmail.com>
2011-02-02 12:27:28 -0800

    [#8992889] Custom views for user sign-up/in.
    We had altered the default Devise views for Users. This moves
    our changes to
      views/users/registrations
    and reverts
      views/devise/registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RoyLevi::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
