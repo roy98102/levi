0d22f7090d47fe2abd1646cc832c004b9f993a21
Christopher Dunn <cdunn2001@gmail.com>
2011-02-04 20:44:28 -0800

    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110205034945) do
+ActiveRecord::Schema.define(:version => 20110205043944) do
 
   create_table "charities", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110205034945) do
     t.string   "site"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "logo"
   end
 
   create_table "users", :force => true do |t|
